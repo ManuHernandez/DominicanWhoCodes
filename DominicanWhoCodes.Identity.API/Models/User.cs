32bb74c792782d8db87a4cf5ab11d75698691ae5
ManuHernandez <hernandezmanuel370@gmail.com>
2019-09-14 16:31:52 -0400

    Adding Identity API service.

---
+++ DominicanWhoCodes.Identity.API/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DominicanWhoCodes.Shared.Users;
+using Microsoft.AspNetCore.Identity;
+
+namespace DominicanWhoCodes.Identity.API.Models
+{
+    public class User : IdentityUser<Guid>, IUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Guid UserId { get { return base.Id; } }
+    }
+}
---
+++ DominicanWhoCodes.Shared/Users/IUser.cs
@@ -0,0 +1,13 @@
+﻿
+
+using System;
+
+namespace DominicanWhoCodes.Shared.Users
+{
+    public interface IUser
+    {
+        Guid UserId { get; }
+        string FirstName { get; set; }
+        string LastName { get; set;}
+    }
+}
